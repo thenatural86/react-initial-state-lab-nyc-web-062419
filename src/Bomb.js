defe6b22567edf5d3b30b88c0f2c58b89b002d22
thenatural86 <philip.smalls86@gmail.com>
2019-08-26 23:00:34 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,19 @@
 // your Bomb code here!
+import React, { PureComponent } from "react"
+
+export default class Bomb extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  render() {
+    const message =
+      this.state.secondsLeft === 0
+        ? "Boom!"
+        : `${this.state.secondsLeft} seconds left before I go boom!`
+
+    return <div>{message}</div>
+  }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,20 @@
 // your ImageSlider code here!
+import React, { PureComponent } from "react"
+
+export default class ImageSlider extends PureComponent {
+  constructor() {
+    super()
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    return (
+      <div className="image-slider">
+        I am on slide {this.state.currentSlideIndex}
+      </div>
+    )
+  }
+}
